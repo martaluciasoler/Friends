ce6ee70a3f0d77d369c48262a72a453be7ce8a7f
Marta Lucía Soler <martadalaber@gmail.com>
2020-07-22 21:35:48 +0200

    estilos añadidos

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
 'use strict';
+
+/*DECLARACIÓN CONSTANTES Y VARIABLES*/
+// let users = datos limpios de la API.
+// let listUsers = selector de cada usuario.
+// const list = selector de <ul>.
+
 const ENDPOINT = 'https://randomuser.me/api/?results=10';
 let users = [];
-let friends = [];
-
-console.log(users);
-
+let listUsers = document.querySelectorAll('.js-userList');
 const list = document.querySelector('.js-list');
+
+/* GETDATA = trae los datos de la API */
+// PUSH objeto (datos limpios) en [] Users.
+// P.D ninguno es amigo, (isfriends = false).
+//llamo a la function RENDER para pintarlos.
+
 function getData() {
   fetch(ENDPOINT)
     .then((response) => response.json())
@@ -23,54 +32,80 @@ function getData() {
 
           login: usersData[i].login.username,
 
-          isFriend: '',
+          isFriend: false,
         });
       }
       renderUsers(users);
-      listeners();
     });
 }
 getData();
 
+/*FUNCTION RENDERUSERS (pinta)*/
+//  list.innerHTML = codeHTML añade a <ul> todos los <li>.
+//  listeners(); se ejecuta una vez se pinten las personas <li>
+// he añadido el nombre del login a cada LISTUSERS con DATA-ID
+
 function renderUsers(array) {
+  let codeHTML = '';
   for (let i = 0; i < array.length; i++) {
-    list.innerHTML += `<li  class="js-userList" id='friend${i}'><img src="${array[i].picture}"><h1>"${array[i].name}"</h1><div>"${array[i].location}"</div><span>"${array[i].login}"</span></li>`;
+    if (array[i].isFriend === true) {
+      codeHTML += `<li  class="js-userList pink list" data-id='${array[i].login}'>`;
+      codeHTML += `<img class="img" src="${array[i].picture}">`;
+      codeHTML += `<h1 class="name">"${array[i].name}"</h1>`;
+      codeHTML += `<div class="city" >"${array[i].location}"</div>`;
+      codeHTML += `<span class= "userName">"${array[i].login}"</span></li>`;
+    } else {
+      codeHTML += `<li  class="js-userList list" data-id='${array[i].login}'>`;
+      codeHTML += `<img class="img" src="${array[i].picture}">`;
+      codeHTML += `<h1 class="name">"${array[i].name}"</h1>`;
+      codeHTML += `<div class="city">"${array[i].location}"</div>`;
+      codeHTML += `<span class= "userName">"${array[i].login}"</span></li>`;
+    }
   }
+  list.innerHTML = codeHTML;
+
+  listeners();
 }
 
-//Cuando se haga click en uno de los usuarios del listado hay que:
-//1. En el objeto del usuario clickado dentro del array, hay que añadir una propiedad para marcarlo como amigo, por ejemplo poniendo isFriend: true.
-//2. Volver a pintar el listado de usuarios en pantalla: 1. Comprobar si cada usuario pintado es un amigo y en caso afirmativo pintar el color de fondo de otro color, por ejemplo rosa.
+/*FUNCTION LISTENERS para escuchas a las personas <li> */
+
 function listeners() {
-  const listUsers = document.querySelectorAll('.js-userList');
+  listUsers = document.querySelectorAll('.js-userList');
   for (const li of listUsers) {
     li.addEventListener('click', friendUser);
   }
 }
 
+/*FUNCTION FRIENDUSER*/
+// añade o quita clase "pink" del elemento clickado.
+//const loginNameListUser = login de cada elemento clickado (DATA-ID) para poder identificar con que elemento del [] USER se corresponde.
+//const arrayLoginUsers = con MAP estoy creando un [] igual de largo que USERS que contiene  solo el nombre del login(nombre del usuario).
+//const userIndex = me da el indice del [] arrayLoginUsers ( que solo contiene strings con el login de cada persona) que coincide con el login de la persona clickeada (DATA-ID)
+// L93.accedo al elemento del [] USERS que tiene el INDICE USERINDEX, a su propiedad ISFRIEND y machacho el dato poniendo "TRUE" (orden de los indices de [] USERS = orden de los indices de []arrayLiginUsers)
+
 function friendUser(event) {
-  const target = event.currentTarget;
-  addToFriends(target);
+  event.currentTarget.classList.toggle('pink');
+  const loginNameListUser = event.currentTarget.dataset.id;
+  const arrayLoginUsers = users.map((user) => user.login);
+  const userIndex = arrayLoginUsers.findIndex(
+    (elem) => elem === loginNameListUser
+  );
+  users[userIndex].isFriend = true;
 }
 
-function addToFriends(ev) {
-  const friendName = ev.querySelector('span').innerHTML;
-  console.log(friendName);
-  const elemIndex = friends.findIndex((elem) => elem.login === friendName);
+/*LOCAL STORAGE*/
 
-  if (elemIndex === -1) {
-    ev.classList.add('pink');
-    const userFriend = users.find((user) => user.login === friendName);
-    friends.push(friendName);
-    console.log(userFriend);
-    console.log(friends);
-  }
+const buttonSetData = document.querySelector('.js-setData');
+buttonSetData.addEventListener('click', setInToLocal);
+
+function setInToLocal() {
+  localStorage.setItem('usersFriends', JSON.stringify(users));
 }
 
-function saveInToLocal() {
-  localStorage.setItem('userFriends', JSON.stringify(friends));
-}
+const buttonGetData = document.querySelector('.js-getData');
+buttonGetData.addEventListener('click', getFromLocal);
 
 function getFromLocal() {
-  return JSON.parse(localStorage.getItem('userFriends'));
+  const localFriends = JSON.parse(localStorage.getItem('usersFriends'));
+  renderUsers(localFriends); // llamo a FUNCTION RENDERUSER y le doy como parametro ( LOCALFRIENDS (personas guardadas en LOCAL ESTORAGE))
 }
